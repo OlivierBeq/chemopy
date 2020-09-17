3b2d7572c232dda3d35cfae2baaef9d6fbbc1fc7
OlivierBeq <35777118+OlivierBeq@users.noreply.github.com>
2020-09-18 00:56:16 +0200

    Support for MOPAC
    Add MOPAC 7.1 (public domain) and support for other MOPAC versions with configuration file

--- src/chemopy/GeoOpt.py
+++ src/chemopy/GeoOpt.py
@@ -5,10 +5,22 @@
 
 
 import os
+import shutil
+import subprocess  # noqa: S404
+import tempfile
+import warnings
+from typing import List, Tuple, Union
 
 from openbabel import pybel
+from rdkit import Chem
+from rdkit.Chem import rdDistGeom
 
 from pychem import vector3d
+from pychem.external import ExternalToolsParser
+from pychem.utils import (GetFileInDirFromExt, GetLastestCreatedFile,
+                          IsInSubdirectoryTree)
+
+MOPAC_CONFIG = {}
 
 
 class Atom:
@@ -97,50 +109,261 @@ def _ReadCoordinates(arc_file):
     return res
 
 
-def FormatConversion(inputmol: Chem.Mol) -> Tuple[str, str]:
-    """Prepare a molecule to be optimized by MOPAC."""
-    #inputmol.removeh()
-    inputmol.addh()
-    inputmol.make3D(forcefield='mmff94', steps=50)    ##Gemetrical optimization
-    ##forcefields = ['uff', 'mmff94', 'ghemical']
-    #make3D(self, forcefield = "mmff94", steps = 50)
-    ##inputmol.localopt(forcefield='mmff94',steps=50)
-    outputmol = pybel.Outputfile('mop', "temp.dat", overwrite=True)
-    outputmol.write(inputmol)
+def FormatConversion(inputmol: Chem.Mol, method='AM1', version='7.1', outfile=None, outdir=None, sdf=False) -> Tuple[str, str]:
+    """Prepare a molecule to be optimized by MOPAC.
+
+    The preparation goes as follows:
+
+    1. all hydrogens are made explicit,
+    2. a 3D conformer is generated from RDKit's stochastic search,
+       based on distance geometry and  exerimental crystallographic knowledge,
+       Wang S., J. Chem. Inf. Model. (2020), 60(4),2044-2058.
+    3. the conformer is converted to a first MOPAC input file with OpenBabel,
+    4. semi-empirical method and MOPAC version is added to the MOPAC input file.
+
+    A SDF can also be written to disk from the generated conformer to ease debugging.
+
+    :param method: MOPAC semi-empirical method to be used for molecular geometry oprimization
+    :param version: version of MOPAC to use
+    :param outfile: name of the output the MOPAC input file
+    :param outdir: directory where to create the MOPAC input file
+        If not specified, a temporary directory is created but
+        THE USER MUST REMOVE IT THEMSELVES USING GeOpt.Dispose.
+    :param sdf: whether a sdf file should also be created for easier debugging.
+    :return: the directory where the MOPAC input file was created
+    """
+    # Ensure method and MOPAC version are compatible
+    if not IsMethodSupportedByMOPAC(method, version):
+        raise ValueError(f'Method {method} is not supported by MOPAC {version}.')
+    # Step 1: add Hs
+    inputmol = Chem.AddHs(inputmol)
+    # Step 2: generate conformer
+    rdDistGeom.EmbedMolecule(inputmol, rdDistGeom.ETKDGv3())
+    pybelmol = pybel.readstring('sdf', Chem.MolToMolBlock(inputmol))
+    running_dir = tempfile.mkdtemp() if outdir is None else outdir
+    mpo_name = 'temp' if outfile is None else outfile
+    if sdf:
+        outputmol = pybel.Outputfile('sdf', os.path.join(running_dir, f'{mpo_name}.sdf'), overwrite=True)
+        outputmol.write(pybelmol)
+        outputmol.close()
+    # Step 3: create first MOPAC input file
+    outputmol = pybel.Outputfile('mop', os.path.join(running_dir, f'{mpo_name}.dat'), overwrite=True)
+    outputmol.write(pybelmol)
     outputmol.close()
-    f = file('temp.dat','r+')
-    f.write('AM1              ')
-    f.close()
+    # Step 4: add additional information to MOPAC input file
+    with open(os.path.join(running_dir, f'{mpo_name}.dat'), 'r') as f:
+        data = f.readlines()
+    data[0] = method + (' PRTCHAR \n' if version == '2016' else '  \n')
+    with open(os.path.join(running_dir, f'{mpo_name}.dat'), 'w') as f:
+        f.write("".join(data))
+    return running_dir, f'{mpo_name}.dat'
 
 
-def RunMOPAC(filename: str) -> int:
-    """Run the MOPAC on a well prepared input file."""
-    
-    itest=os.system("run_mopac7"+" "+filename)
-    #time.sleep(1)
-    return itest
+def MopacCfgParser(path: str = None) -> None:
+    """Read the MOPAC configuration file and locate executables.
 
-############################################################################ 
-def GetARCFile(inputmol: Chem.Mol) -> None:
+    :param path: path to MOPACc configuration file.
     """
-    #################################################################
-    Get ARC file for each molecule
-    #################################################################
+    global MOPAC_CONFIG
+    if path is None:
+        path = os.path.join(os.path.dirname(__file__), 'MOPAC.cfg')
+    etp = ExternalToolsParser(path, ['version', 'path', 'bin', 'methods'], False)
+    for _, section in etp.tools.items():
+        version_, path_, bin_, methods_ = (section['version'], section['path'],
+                                           section['bin'], section['methods'])
+        methods_ = [x.strip() for x in methods_.split()]
+        # Make sure path is right
+        if path_.startswith('.'):
+            fullpath_ = os.path.realpath(os.path.join(os.path.dirname(__file__), path_, bin_))
+        else:
+            fullpath_ = os.path.realpath(os.path.join(path_, bin_))
+        # Make sure executable exists
+        if not os.path.isfile(fullpath_):
+            warnings.warn(f'Wrong path in MOPAC config file: MOPAC(v{version_}) -> {fullpath_}', ResourceWarning)
+        else:
+            MOPAC_CONFIG[version_] = [fullpath_, methods_]
+    if not MOPAC_CONFIG:  # Empty or corrupted config file
+        raise ValueError('MOPAC config file is either empty or corrupted and needs to be edited.')
+
+
+def RunMOPAC(filename: str, version: str = '7.1') -> int:
+    """Run the MOPAC on a well prepared input file.
+
+    Parse default MOPAC config file if not read already.
+
+    :param filename: path to the well prepared MOPAC input file
+    :param version: MOPAC version to be used
     """
-    
-    FormatConversion(inputmol)
-    itest = RunMOPAC('temp')
-    if not itest:
-        print(itest,'\t', 'Finshed successfully!')
+    # Ensure all requirements are set
+    if not IsMOPACVersionAvailable(version):
+        raise ValueError(f'MOPAC version {version} is not available. Check your MOPAC config file.')
+    # Run optimization
+    mopac_bin = MOPAC_CONFIG[str(version)][0]
+    try:
+        retcode = subprocess.call(f'{mopac_bin} {filename}', shell=False)  # noqa: S603
+        return retcode
+    except Exception:
+        return 1
+
+
+def Dispose(dir_: str, force: bool = False) -> None:
+    """Properly dispose of a temporary folder.
+
+    :param force: whether to allow removing a non temporary directory.
+    """
+    # If neither is dir_ in tempdir nor force set to True
+    if not (IsInSubdirectoryTree(dir_, tempfile.gettempdir()) or force):
+        raise PermissionError('Directory is not temporary. If you know what you '
+                              'are doing force the  deletion')
+    shutil.rmtree(dir_, ignore_errors=True)
+
+
+def IsMOPACVersionAvailable(version: str) -> bool:
+    """Return if the dsired version of MOPAC can be used.
+
+    :param version: version of MOPAC
+    """
+    if not MOPAC_CONFIG:
+        MopacCfgParser()
+    return str(version) in MOPAC_CONFIG.keys()
+
+
+def IsMethodSupportedByMOPAC(method: str, version: str) -> bool:
+    """Return if the version of MOPAC supports a specific method.
+
+    :param method: semi-empirical method to be applied
+    :param version: version of MOPAC
+    """
+    if not IsMOPACVersionAvailable(version):
+        raise ValueError(f'MOPAC version {version} is not available. Check your MOPAC config file.')
+    return str(method) in MOPAC_CONFIG[f'{version}'][1]
+
+
+def GetARCFile(inputmol: Chem.Mol, method: str = 'PM3', version: str = '7.1',
+               verbose: bool = True, exit_on_fail: bool = False,
+               ) -> Union[Tuple[str, str], None]:
+    """Optimize molecule geometry with MOPAC.
+
+    :param inputmol: molecule to optimize
+    :param method: semi-empirical method to apply
+    :param version: version of MOPAC to be used
+    :param verbose: whether to print progress messages
+    :param exit_on_fail: if False, if a method fails at generating
+                         a structure, others are tried from most to
+                         least accurate.
+                         if True, return False on failure.
+    :return: Tuple of (path to folder, path to arc_file) on success,
+             None otherwise.
+    """
+    # Create proper input file
+    dir_, dat_file = FormatConversion(inputmol, method, version)
+    # Ensure dat file exists
+    full_path = os.path.join(dir_, dat_file)
+    if not os.path.isfile(full_path):
+        raise FileNotFoundError('Molecule could not be prepared for MOPAC.')
+    # Run MOPAC
+    retcode = RunMOPAC(full_path, version)
+    # Get generated file
+    # Different versions of MOPAC handle the outputname differently
+    # e.g. 7.1 appends .arc after the .dat giving a .dat.arc file
+    # while 2016 replaces the .dat by .arc
+    output = GetFileInDirFromExt(dir_, '.arc')
+    # Success when return code is 0
+    success = not retcode and len(output) > 0
+    if success:
+        if verbose:
+            print(f'Molecule geometry was successfully optimized using {method}.')  # noqa T001
+        output = output[0] if len(output) == 1 else GetLastestCreatedFile(filepaths=output)
+        # Rename output
+        curated_filename = f'{os.path.splitext(full_path)[0]}.arc'
+        os.rename(output, curated_filename)
+        return dir_, curated_filename
+    elif exit_on_fail:
+        if verbose:
+            print(f'Geometry optimization failed with {method}')  # noqa T001
+        # if IsInSubdirectoryTree(dir_, tempfile.gettempdir()):
+            # Dispose(dir_)
+        return
+    else:  # neither success nor exit_on_fail
+        if verbose:
+            print(f'Geometry optimization failed with {method}')  # noqa T001
+        # if IsInSubdirectoryTree(dir_, tempfile.gettempdir()):
+            # Dispose(dir_)
+        methods_tried = MOPAC_CONFIG[f'{version}'][1]
+        # Remove the method that was just used
+        methods_tried.remove(method)
+        # Try all possible methods, from most to least accurate
+        skip_first_time = True
+        while not success:
+            if not skip_first_time:
+                del methods_tried[0]  # Remove method tried to allow going to the next
+                if len(methods_tried) == 0:  # No method left
+                    if verbose:
+                        print('Molecule could not be optimized.')  # noqa T001
+                    else:
+                        warnings.warn('Molecule could not be optimized', RuntimeWarning)
+                    return
+            skip_first_time = False
+            if verbose:
+                print(f'Attempting optimization with {methods_tried[0]}')  # noqa T001
+            # Create proper input file
+            new_attempt = GetARCFile(inputmol, methods_tried[0], version, verbose, exit_on_fail=True)
+            if new_attempt is not None:
+                return new_attempt
+
+
+def GetOptimizedMol(arc_file: str = None, inputmol: Chem.Mol = None,
+                    method: str = 'PM3', version: str = '7.1',
+                    verbose: bool = False, dispose: bool = True,
+                    ) -> Union[pybel.Molecule, None]:
+    """Optimize molecule geometry with MOPAC and return the optimized molecule.
+
+    In order not to optimize molecule multiple times, an ARC file may be provided.
+    The path to the MOPAC output file is determined based on the one of the provided
+    ARC file.
+
+    If not already optimized, a molecule may be provided.
+
+    :param arc_file: Path to MOPAC .arc file
+                     (ignored if inputmol provided).
+    :param inputmol: molecule to optimize
+                     (ignored if arc_file provided).
+    :param method: semi-empirical method to apply
+                   (ignored if arc_file provided).
+    :param version: version of MOPAC to be used
+                    (ignored if arc_file provided).
+    :param verbose: whether to print progress messages
+                    (ignored if arc_file provided).
+    :param dispose: whether to remove generated MOPAC output files
+                    (ignored if arc_file provided).
+    :return: optimized rdkit molecule on success, None otherwise.
+    """
+    if arc_file is None and inputmol is None:
+        raise ValueError('Either ARC file or inputmolecule must be provided.')
+    if arc_file is not None:
+        mopac_out_dir = os.path.dirname(arc_file)
+        mopac_out_path = GetFileInDirFromExt(mopac_out_dir, '.out')
+        mopac_out_path = mopac_out_path[0] if len(mopac_out_path) == 1 \
+            else GetLastestCreatedFile(filepaths=mopac_out_path)
+        if not len(mopac_out_path):
+            return None
+        pybelmol = next(pybel.readfile('mopout', mopac_out_path))
+        return Chem.MolFromMol2Block(pybelmol.write(format='mol2'))
     else:
-        print(itest,'\t', 'Failed Finished........')
-    os.remove('temp.dat')
-    os.remove('temp.log')
-    os.remove('temp.OUT')    
-    #os.remove('temp.arc')
-    oldpath = os.getcwd() + '/temp.arc'
-    newpath = os.getcwd() + '/temp'
-    os.rename(oldpath, newpath)
+        res = GetARCFile(inputmol, method, version, verbose, False)
+        if res is None:
+            return None
+        dir_, arc_file_ = res
+        mopac_out_path = GetFileInDirFromExt(dir_, '.out')
+        mopac_out_path = mopac_out_path[0] if len(mopac_out_path) == 1 \
+            else GetLastestCreatedFile(filepaths=mopac_out_path)
+        if not len(mopac_out_path):
+            return None
+        pybelmol = next(pybel.readfile('mopout', mopac_out_path))
+        if dispose:
+            Dispose(dir_)
+        return pybelmol
+        # return Chem.MolFromMol2Block(pybelmol.write(format='mol2'), removeHs=False)
 
 
 ##############################################################################
