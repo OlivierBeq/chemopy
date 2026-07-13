9a9279e6dea73f7ce2fa995c9b36bdf5b18a9840
OlivierBeq <35777118+OlivierBeq@users.noreply.github.com>
2026-07-14 01:39:45 +0200

    Fix CAS lookup crash and mock network-dependent tests

--- src/chemopy/getmol.py
+++ src/chemopy/getmol.py
@@ -16,17 +16,20 @@ class MolFrom:
     def CAS(cas_id: str = "") -> Chem.Mol:
         """Download molecule from ChemNet by CAS ID."""
         casid = cas_id.strip()
-        request = urllib.request.Request(f'http://www.chemnet.com/cas/supplier.cgi?terms={casid}&l=&exact=dict')
+        request = urllib.request.Request(f"http://www.chemnet.com/cas/supplier.cgi?terms={casid}&l=&exact=dict")
         with urllib.request.urlopen(request) as response:
             temp = [line.decode("utf-8") for line in response.readlines()]
+        res = None
         for i in temp:
-            if re.findall('InChI=', i) == ['InChI=']:
+            if re.findall("InChI=", i) == ["InChI="]:
                 k = i.split('    <td align="left">')
-                kk = k[1].split('</td>\r\n')
+                kk = k[1].split("</td>\r\n")
                 if kk[0][0:5] == "InChI":
                     res = kk[0]
                 else:
                     res = "None"
+        if res is None:
+            raise ValueError(f"Could not find an InChI for CAS ID {casid!r} in the ChemNet response.")
         m = Chem.MolFromInchi(res.strip())
         return m
 
@@ -34,23 +37,25 @@ class MolFrom:
     def EBI(chebi_id: str = "") -> Chem.Mol:
         """Donwload molecule from ChEBI or ChEMBL using ChEBI or ChEMBL id."""
         chid = chebi_id.strip().upper()
-        if chid.startswith('CHEBI'):
-            request = urllib.request.Request(f'https://www.ebi.ac.uk/chebi/saveStructure.do?sdf=true&chebiId={chid}')
+        if chid.startswith("CHEBI"):
+            request = urllib.request.Request(f"https://www.ebi.ac.uk/chebi/saveStructure.do?sdf=true&chebiId={chid}")
             with urllib.request.urlopen(request) as response:
                 sdf = response.read()
             if not len(sdf):
-                raise Exception(f'Not a valid ChEBI ID: {chid}')
-        elif chid.startswith('CHEMBL'):
-            request = urllib.request.Request(f'https://www.ebi.ac.uk/chembl/api/data/molecule?chembl_id={chid}&format=sdf')
+                raise Exception(f"Not a valid ChEBI ID: {chid}")
+        elif chid.startswith("CHEMBL"):
+            request = urllib.request.Request(
+                f"https://www.ebi.ac.uk/chembl/api/data/molecule?chembl_id={chid}&format=sdf"
+            )
             with urllib.request.urlopen(request) as response:
                 sdf = response.read().decode()
             # xml_tree = ET.fromstring(xml)
             # structure = xml_tree.findall('./molecules/molecule/molecule_structures/molfile')
             if not len(sdf):
-                raise Exception(f'Not a valid ChEMBL ID: {chid}')
+                raise Exception(f"Not a valid ChEMBL ID: {chid}")
             # sdf = structure[0].text
         else:
-            raise Exception('Valid ID starts with CHEBI: or CHEMBL')
+            raise Exception("Valid ID starts with CHEBI: or CHEMBL")
         m = Chem.MolFromMolBlock(sdf)
         return m
 
@@ -58,8 +63,10 @@ class MolFrom:
     def NCBI(cid: str = "") -> Chem.Mol:
         """Download molecule from PubChem using PubChem CID."""
         cid = cid.strip()
-        cid = cid.upper().replace('CID', '') if 'CID' in cid.upper() else cid
-        request = urllib.request.Request(f'http://pubchem.ncbi.nlm.nih.gov/summary/summary.cgi?cid={cid}&disopt=SaveSDF')
+        cid = cid.upper().replace("CID", "") if "CID" in cid.upper() else cid
+        request = urllib.request.Request(
+            f"http://pubchem.ncbi.nlm.nih.gov/summary/summary.cgi?cid={cid}&disopt=SaveSDF"
+        )
         with urllib.request.urlopen(request) as response:
             m = Chem.MolFromMolBlock(response.read())
         return m
@@ -68,11 +75,15 @@ class MolFrom:
     def DrugBank(drugbank_id: str = "") -> Chem.Mol:
         """Download molecule from DrugBank using DrugBank id."""
         dbid = drugbank_id.strip()
-        request = urllib.request.Request(f'https://go.drugbank.com/structures/small_molecule_drugs/{dbid}.sdf',
-                                         headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) "
-                                                                "AppleWebKit/537.36 (KHTML, like Gecko) "
-                                                                "Chrome/39.0.2171.95 "
-                                                                "Safari/537.36"})
+        request = urllib.request.Request(
+            f"https://go.drugbank.com/structures/small_molecule_drugs/{dbid}.sdf",
+            headers={
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) "
+                "AppleWebKit/537.36 (KHTML, like Gecko) "
+                "Chrome/39.0.2171.95 "
+                "Safari/537.36"
+            },
+        )
         with urllib.request.urlopen(request) as response:
             m = Chem.MolFromMolBlock(response.read())
         return m
@@ -81,7 +92,7 @@ class MolFrom:
     def KEGG(kegg_id: str = ""):
         """Download molecule from KEGG using KEGG id."""
         ID = str(kegg_id)
-        request = urllib.request.Request(f'http://www.genome.jp/dbget-bin/www_bget?-f+m+drug+{ID}')
+        request = urllib.request.Request(f"http://www.genome.jp/dbget-bin/www_bget?-f+m+drug+{ID}")
         with urllib.request.urlopen(request) as response:  # nosec: S310
             m = Chem.MolFromMolBlock(response.read())
         return m
--- tests/test_fetchmols.py
+++ tests/test_fetchmols.py
@@ -1,43 +1,97 @@
 # -*- coding: utf-8 -*-
 
-"""Tests for the fetching of molecules from online resources."""
+"""Tests for molecule retrieval from online resources (network calls are mocked)."""
 
 import unittest
+from contextlib import contextmanager
+from unittest import mock
+
+from rdkit import Chem
 
 from chemopy import MolFrom
 
-from tests.constants import *
+_ETHANOL_MOLBLOCK = Chem.MolToMolBlock(Chem.MolFromSmiles("CCO"))
+
+_CAS_RESPONSE_LINES = [
+    b"<html>\r\n",
+    b'    <td align="left">InChI=1S/C2H6O/c1-2-3/h3H,2H2,1H3</td>\r\n',
+    b"</html>\r\n",
+]
 
 
-class TestDescriptorsAndFingerprints(unittest.TestCase):
-    """Tests for online molecule retrieval."""
+def _fake_urlopen(read_value: bytes = None, readlines_value=None):
+    """Build a context-manager-compatible fake for urllib.request.urlopen."""
 
-    def test_cas(self):
-        for casnum in CAS_NUMBERS:
-            mol = MolFrom.CAS(casnum)
-            self.assertIsNotNone(mol)
-            self.assertNotEqual(Chem.MolToSmiles(mol), '')
+    @contextmanager
+    def _cm(*args, **kwargs):
+        response = mock.Mock()
+        if read_value is not None:
+            response.read.return_value = read_value
+        if readlines_value is not None:
+            response.readlines.return_value = readlines_value
+        yield response
 
-    def test_ebi(self):
-        for ebinum in EBI_NUMBERS:
-            mol = MolFrom.EBI(ebinum)
-            self.assertIsNotNone(mol)
-            self.assertNotEqual(Chem.MolToSmiles(mol), '')
+    return _cm
 
+
+class TestMolFromCAS(unittest.TestCase):
+    def test_cas_valid_response(self):
+        with mock.patch("urllib.request.urlopen", side_effect=_fake_urlopen(readlines_value=_CAS_RESPONSE_LINES)):
+            mol = MolFrom.CAS("64-17-5")
+        self.assertIsNotNone(mol)
+        self.assertNotEqual(Chem.MolToSmiles(mol), "")
+
+    def test_cas_no_inchi_in_response_raises_clear_error(self):
+        with mock.patch("urllib.request.urlopen", side_effect=_fake_urlopen(readlines_value=[b"<html></html>\r\n"])):
+            with self.assertRaises(ValueError):
+                MolFrom.CAS("64-17-5")
+
+
+class TestMolFromEBI(unittest.TestCase):
+    def test_ebi_chebi_id(self):
+        with mock.patch("urllib.request.urlopen", side_effect=_fake_urlopen(read_value=_ETHANOL_MOLBLOCK.encode())):
+            mol = MolFrom.EBI("CHEBI:16236")
+        self.assertIsNotNone(mol)
+        self.assertNotEqual(Chem.MolToSmiles(mol), "")
+
+    def test_ebi_chembl_id(self):
+        with mock.patch("urllib.request.urlopen", side_effect=_fake_urlopen(read_value=_ETHANOL_MOLBLOCK.encode())):
+            mol = MolFrom.EBI("CHEMBL545")
+        self.assertIsNotNone(mol)
+
+    def test_ebi_invalid_id_raises(self):
+        with self.assertRaises(Exception):
+            MolFrom.EBI("NOTVALID")
+
+    def test_ebi_empty_response_raises(self):
+        with mock.patch("urllib.request.urlopen", side_effect=_fake_urlopen(read_value=b"")):
+            with self.assertRaises(Exception):
+                MolFrom.EBI("CHEBI:16236")
+
+
+class TestMolFromNCBI(unittest.TestCase):
     def test_ncbi(self):
-        for ncbinum in NCBI_NUMBERS:
-            mol = MolFrom.NCBI(ncbinum)
-            self.assertIsNotNone(mol)
-            self.assertNotEqual(Chem.MolToSmiles(mol), '')
+        with mock.patch("urllib.request.urlopen", side_effect=_fake_urlopen(read_value=_ETHANOL_MOLBLOCK.encode())):
+            mol = MolFrom.NCBI("CID702")
+        self.assertIsNotNone(mol)
+        self.assertNotEqual(Chem.MolToSmiles(mol), "")
 
+
+class TestMolFromDrugBank(unittest.TestCase):
     def test_drugbank(self):
-        for drugbankid in DRUGBANK_NUMBERS:
-            mol = MolFrom.DrugBank(drugbankid)
-            self.assertIsNotNone(mol)
-            self.assertNotEqual(Chem.MolToSmiles(mol), '')
+        with mock.patch("urllib.request.urlopen", side_effect=_fake_urlopen(read_value=_ETHANOL_MOLBLOCK.encode())):
+            mol = MolFrom.DrugBank("DB00898")
+        self.assertIsNotNone(mol)
+        self.assertNotEqual(Chem.MolToSmiles(mol), "")
 
+
+class TestMolFromKEGG(unittest.TestCase):
     def test_kegg(self):
-        for keggid in KEGG_NUMBERS:
-            mol = MolFrom.KEGG(keggid)
-            self.assertIsNotNone(mol)
-            self.assertNotEqual(Chem.MolToSmiles(mol), '')
+        with mock.patch("urllib.request.urlopen", side_effect=_fake_urlopen(read_value=_ETHANOL_MOLBLOCK.encode())):
+            mol = MolFrom.KEGG("D00013")
+        self.assertIsNotNone(mol)
+        self.assertNotEqual(Chem.MolToSmiles(mol), "")
+
+
+if __name__ == "__main__":
+    unittest.main()
