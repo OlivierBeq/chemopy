cd02b8338087b2a4883c0cff1cfead08a4295cdc
OlivierBeq <35777118+OlivierBeq@users.noreply.github.com>
2026-07-14 01:44:46 +0200

    Optimize connectivity descriptor calculation

--- src/chemopy/connectivity.py
+++ src/chemopy/connectivity.py
@@ -17,13 +17,12 @@ class Connectivity:
     """Topological indices"""
 
     @staticmethod
-    def calculate_chi0(mol: Chem.Mol) -> float:
+    def calculate_chi0(mol: Chem.Mol, deltas=None) -> float:
         """Calculate molecular connectivity chi index for path order 0."""
-        deltas = [x.GetDegree() for x in mol.GetAtoms()]
-        deltas = np.array(deltas, 'd')
+        if deltas is None:
+            deltas = np.array([x.GetDegree() for x in mol.GetAtoms()], dtype=float)
         deltas = deltas[deltas != 0]
-        res = sum(np.sqrt(1. / deltas))
-        return res
+        return float(np.sum(np.sqrt(1.0 / deltas)))
 
     @staticmethod
     def calculate_chi1(mol: Chem.Mol) -> float:
@@ -33,8 +32,8 @@ class Connectivity:
             return 0.0
         while 0 in cc:
             cc.remove(0)
-        cc = np.array(cc, 'd')
-        res = sum(np.sqrt(1. / cc))
+        cc = np.array(cc, "d")
+        res = sum(np.sqrt(1.0 / cc))
         return res
 
     @staticmethod
@@ -45,22 +44,21 @@ class Connectivity:
             return 0.0
         while 0 in cc:
             cc.remove(0)
-        cc = np.array(cc, 'd')
-        res = np.mean(np.sqrt(1. / cc))
+        cc = np.array(cc, "d")
+        res = np.mean(np.sqrt(1.0 / cc))
         return res
 
     @staticmethod
-    def _calculate_chinp(mol: Chem.Mol, NumPath: int = 2) -> float:
+    def _calculate_chinp(mol: Chem.Mol, NumPath: int = 2, deltas=None) -> float:
         """Calculate molecular connectivity chi index for path order 2."""
-        accum = 0.0
-        deltas = [x.GetDegree() for x in mol.GetAtoms()]
-        for path in Chem.FindAllPathsOfLengthN(mol, NumPath + 1, useBonds=0):
-            cAccum = 1.0
-            for idx in path:
-                cAccum *= deltas[idx]
-            if cAccum:
-                accum += 1. / np.sqrt(cAccum)
-        return accum
+        if deltas is None:
+            deltas = np.array([x.GetDegree() for x in mol.GetAtoms()], dtype=float)
+        paths = Chem.FindAllPathsOfLengthN(mol, NumPath + 1, useBonds=0)
+        if len(paths) == 0:
+            return 0.0
+        products = deltas[np.array(paths)].prod(axis=1)
+        products = products[products != 0]
+        return float(np.sum(1.0 / np.sqrt(products)))
 
     @staticmethod
     def calculate_chi2(mol: Chem.Mol) -> float:
@@ -111,7 +109,7 @@ class Connectivity:
     def calculate_chi3c(mol: Chem.Mol) -> float:
         """Calculate molecular connectivity chi index for cluster."""
         accum = 0.0
-        patt = Chem.MolFromSmarts('*~*(~*)~*')
+        patt = Chem.MolFromSmarts("*~*(~*)~*")
         HPatt = mol.GetSubstructMatches(patt)
         for cluster in HPatt:
             deltas = [mol.GetAtomWithIdx(x).GetDegree() for x in cluster]
@@ -119,14 +117,14 @@ class Connectivity:
                 deltas.remove([0])
             if deltas != []:
                 deltas1 = np.array(deltas, dtype=float)
-                accum = accum + 1. / np.sqrt(deltas1.prod())
+                accum = accum + 1.0 / np.sqrt(deltas1.prod())
         return accum
 
     @staticmethod
     def calculate_chi4c(mol: Chem.Mol) -> float:
         """Calculate molecular connectivity chi index for cluster."""
         accum = 0.0
-        patt = Chem.MolFromSmarts('*~*(~*)(~*)~*')
+        patt = Chem.MolFromSmarts("*~*(~*)(~*)~*")
         HPatt = mol.GetSubstructMatches(patt)
         for cluster in HPatt:
             deltas = [mol.GetAtomWithIdx(x).GetDegree() for x in cluster]
@@ -134,14 +132,14 @@ class Connectivity:
                 deltas.remove([0])
             if deltas != []:
                 deltas1 = np.array(deltas, dtype=float)
-                accum = accum + 1. / np.sqrt(deltas1.prod())
+                accum = accum + 1.0 / np.sqrt(deltas1.prod())
         return accum
 
     @staticmethod
     def calculate_chi4pc(mol: Chem.Mol) -> float:
         """Calculate molecular connectivity chi index for path/cluster."""
         accum = 0.0
-        patt = Chem.MolFromSmarts('*~*(~*)~*~*')
+        patt = Chem.MolFromSmarts("*~*(~*)~*~*")
         HPatt = mol.GetSubstructMatches(patt)
         for cluster in HPatt:
             deltas = [mol.GetAtomWithIdx(x).GetDegree() for x in cluster]
@@ -149,7 +147,7 @@ class Connectivity:
                 deltas.remove([0])
             if deltas != []:
                 deltas1 = np.array(deltas, dtype=float)
-                accum = accum + 1. / np.sqrt(deltas1.prod())
+                accum = accum + 1.0 / np.sqrt(deltas1.prod())
         return accum
 
     @staticmethod
@@ -158,18 +156,16 @@ class Connectivity:
         return abs(Connectivity.calculate_chi3c(mol) - Connectivity.calculate_chi4pc(mol))
 
     @staticmethod
-    def _calculate_chinch(mol: Chem.Mol, NumCycle=3) -> float:
+    def _calculate_chinch(mol: Chem.Mol, NumCycle=3, deltas=None) -> float:
         """Calculate molecular connectivity chi index for cycles of n."""
-        accum = 0.0
-        deltas = [x.GetDegree() for x in mol.GetAtoms()]
-        for tup in mol.GetRingInfo().AtomRings():
-            cAccum = 1.0
-            if len(tup) == NumCycle:
-                for idx in tup:
-                    cAccum *= deltas[idx]
-                if cAccum:
-                    accum += 1. / np.sqrt(cAccum)
-        return accum
+        if deltas is None:
+            deltas = np.array([x.GetDegree() for x in mol.GetAtoms()], dtype=float)
+        rings = [tup for tup in mol.GetRingInfo().AtomRings() if len(tup) == NumCycle]
+        if not rings:
+            return 0.0
+        products = deltas[np.array(rings)].prod(axis=1)
+        products = products[products != 0]
+        return float(np.sum(1.0 / np.sqrt(products)))
 
     @staticmethod
     def calculate_chi3ch(mol: Chem.Mol) -> float:
@@ -192,27 +188,24 @@ class Connectivity:
         return Connectivity._calculate_chinch(mol, NumCycle=6)
 
     @staticmethod
-    def calculate_chiv0(mol: Chem.Mol) -> float:
+    def calculate_chiv0(mol: Chem.Mol, deltas=None) -> float:
         """Calculate valence molecular connectivity chi index for path order 0."""
-        deltas = Topology._hall_kier_deltas(mol, skipHs=0)
-        while 0 in deltas:
-            deltas.remove(0)
-        deltas = np.array(deltas, 'd')
-        res = sum(np.sqrt(1. / deltas))
-        return res
+        if deltas is None:
+            deltas = np.array(Topology._hall_kier_deltas(mol, skipHs=0), dtype=float)
+        deltas = deltas[deltas != 0]
+        return float(np.sum(np.sqrt(1.0 / deltas)))
 
     @staticmethod
-    def _calculate_chivnp(mol: Chem.Mol, NumPath: int = 1) -> float:
+    def _calculate_chivnp(mol: Chem.Mol, NumPath: int = 1, deltas=None) -> float:
         """Calculate valence molecular connectivity chi index for path order 1."""
-        accum = 0.0
-        deltas = Topology._hall_kier_deltas(mol, skipHs=False)
-        for path in Chem.FindAllPathsOfLengthN(mol, NumPath + 1, useBonds=0):
-            cAccum = 1.0
-            for idx in path:
-                cAccum *= deltas[idx]
-            if cAccum:
-                accum += 1. / np.sqrt(cAccum)
-        return accum
+        if deltas is None:
+            deltas = np.array(Topology._hall_kier_deltas(mol, skipHs=False), dtype=float)
+        paths = Chem.FindAllPathsOfLengthN(mol, NumPath + 1, useBonds=0)
+        if len(paths) == 0:
+            return 0.0
+        products = deltas[np.array(paths)].prod(axis=1)
+        products = products[products != 0]
+        return float(np.sum(1.0 / np.sqrt(products)))
 
     @staticmethod
     def calculate_chiv1(mol: Chem.Mol) -> float:
@@ -313,7 +306,7 @@ class Connectivity:
     def calculate_chiv3c(mol: Chem.Mol) -> float:
         """Calculate valence molecular connectivity chi index for cluster."""
         accum = 0.0
-        patt = Chem.MolFromSmarts('*~*(~*)~*')
+        patt = Chem.MolFromSmarts("*~*(~*)~*")
         HPatt = mol.GetSubstructMatches(patt)
         for cluster in HPatt:
             deltas = [Connectivity._atom_hall_kier_deltas(mol.GetAtomWithIdx(x)) for x in cluster]
@@ -321,14 +314,14 @@ class Connectivity:
                 deltas.remove([0])
             if deltas != []:
                 deltas1 = np.array(deltas, dtype=float)
-                accum = accum + 1. / np.sqrt(deltas1.prod())
+                accum = accum + 1.0 / np.sqrt(deltas1.prod())
         return accum
 
     @staticmethod
     def calculate_chiv4c(mol: Chem.Mol) -> float:
         """Calculate valence molecular connectivity chi index for cluster."""
         accum = 0.0
-        patt = Chem.MolFromSmarts('*~*(~*)(~*)~*')
+        patt = Chem.MolFromSmarts("*~*(~*)(~*)~*")
         HPatt = mol.GetSubstructMatches(patt)
         for cluster in HPatt:
             deltas = [Connectivity._atom_hall_kier_deltas(mol.GetAtomWithIdx(x)) for x in cluster]
@@ -336,14 +329,14 @@ class Connectivity:
                 deltas.remove([0])
             if deltas != []:
                 deltas1 = np.array(deltas, dtype=float)
-                accum = accum + 1. / np.sqrt(deltas1.prod())
+                accum = accum + 1.0 / np.sqrt(deltas1.prod())
         return accum
 
     @staticmethod
     def calculate_chiv4pc(mol: Chem.Mol) -> float:
         """Calculate valence molecular connectivity chi index for path/cluster."""
         accum = 0.0
-        patt = Chem.MolFromSmarts('*~*(~*)~*~*')
+        patt = Chem.MolFromSmarts("*~*(~*)~*~*")
         HPatt = mol.GetSubstructMatches(patt)
         for cluster in HPatt:
             deltas = [Connectivity._atom_hall_kier_deltas(mol.GetAtomWithIdx(x)) for x in cluster]
@@ -351,7 +344,7 @@ class Connectivity:
                 deltas.remove([0])
             if deltas != []:
                 deltas1 = np.array(deltas, dtype=float)
-                accum = accum + 1. / np.sqrt(deltas1.prod())
+                accum = accum + 1.0 / np.sqrt(deltas1.prod())
         return accum
 
     @staticmethod
@@ -360,18 +353,16 @@ class Connectivity:
         return abs(Connectivity.calculate_chiv3c(mol) - Connectivity.calculate_chiv4pc(mol))
 
     @staticmethod
-    def _calculate_chivnch(mol: Chem.Mol, NumCyc=3) -> float:
+    def _calculate_chivnch(mol: Chem.Mol, NumCyc=3, deltas=None) -> float:
         """Calculate valence molecular connectivity chi index for cycles of n."""
-        accum = 0.0
-        deltas = Topology._hall_kier_deltas(mol, skipHs=0)
-        for tup in mol.GetRingInfo().AtomRings():
-            cAccum = 1.0
-            if len(tup) == NumCyc:
-                for idx in tup:
-                    cAccum *= deltas[idx]
-                if cAccum:
-                    accum += 1. / np.sqrt(cAccum)
-        return accum
+        if deltas is None:
+            deltas = np.array(Topology._hall_kier_deltas(mol, skipHs=0), dtype=float)
+        rings = [tup for tup in mol.GetRingInfo().AtomRings() if len(tup) == NumCyc]
+        if not rings:
+            return 0.0
+        products = deltas[np.array(rings)].prod(axis=1)
+        products = products[products != 0]
+        return float(np.sum(1.0 / np.sqrt(products)))
 
     @staticmethod
     def calculate_chiv3ch(mol: Chem.Mol) -> float:
@@ -395,54 +386,46 @@ class Connectivity:
 
     @staticmethod
     def get_all(mol: Chem.Mol) -> dict:
-        """Get all (44) connectivity descriptors."""
+        """Get all (44) connectivity descriptors.
+
+        Several descriptors below (e.g. `dchi2`/`dchi3`/`dchi4`, `knotp`, `knotpv`) are, by
+        definition, differences of other descriptors already computed here; and the plain/valence
+        Hall-Kier deltas they all depend on are computed once and reused, instead of every single
+        descriptor recomputing them (and refinding the same atom paths/rings) independently.
+        """
+        plain_deltas = np.array([x.GetDegree() for x in mol.GetAtoms()], dtype=float)
+        valence_deltas = np.array(Topology._hall_kier_deltas(mol, skipHs=0), dtype=float)
+
         result = {}
-        for DesLabel in _connectivity.keys():
-            result[DesLabel] = _connectivity[DesLabel](mol)
+        result["Chiv0"] = Connectivity.calculate_chiv0(mol, deltas=valence_deltas)
+        chivnp = {n: Connectivity._calculate_chivnp(mol, NumPath=n, deltas=valence_deltas) for n in range(1, 11)}
+        for n in range(1, 11):
+            result[f"Chiv{n}"] = chivnp[n]
+        result["Chi3c"] = Connectivity.calculate_chi3c(mol)
+        result["Chi4c"] = Connectivity.calculate_chi4c(mol)
+        result["Chi4pc"] = Connectivity.calculate_chi4pc(mol)
+        result["Chi3ch"] = Connectivity._calculate_chinch(mol, NumCycle=3, deltas=plain_deltas)
+        result["Chi4ch"] = Connectivity._calculate_chinch(mol, NumCycle=4, deltas=plain_deltas)
+        result["Chi5ch"] = Connectivity._calculate_chinch(mol, NumCycle=5, deltas=plain_deltas)
+        result["Chi6ch"] = Connectivity._calculate_chinch(mol, NumCycle=6, deltas=plain_deltas)
+        result["Chi0"] = Connectivity.calculate_chi0(mol, deltas=plain_deltas)
+        result["Chi1"] = Connectivity.calculate_chi1(mol)
+        chinp = {n: Connectivity._calculate_chinp(mol, NumPath=n, deltas=plain_deltas) for n in range(2, 11)}
+        for n in range(2, 11):
+            result[f"Chi{n}"] = chinp[n]
+        result["Chiv3c"] = Connectivity.calculate_chiv3c(mol)
+        result["Chiv4c"] = Connectivity.calculate_chiv4c(mol)
+        result["Chiv4pc"] = Connectivity.calculate_chiv4pc(mol)
+        result["Chiv3ch"] = Connectivity._calculate_chivnch(mol, NumCyc=3, deltas=valence_deltas)
+        result["Chiv4ch"] = Connectivity._calculate_chivnch(mol, NumCyc=4, deltas=valence_deltas)
+        result["Chiv5ch"] = Connectivity._calculate_chivnch(mol, NumCyc=5, deltas=valence_deltas)
+        result["Chiv6ch"] = Connectivity._calculate_chivnch(mol, NumCyc=6, deltas=valence_deltas)
+        result["mChi1"] = Connectivity.calculate_mean_randic(mol)
+        result["knotp"] = abs(result["Chi3c"] - result["Chi4pc"])
+        result["dchi0"] = abs(result["Chiv0"] - result["Chi0"])
+        result["dchi1"] = abs(result["Chiv1"] - result["Chi1"])
+        result["dchi2"] = abs(chivnp[2] - chinp[2])
+        result["dchi3"] = abs(chivnp[3] - chinp[3])
+        result["dchi4"] = abs(chivnp[4] - chinp[4])
+        result["knotpv"] = abs(result["Chiv3c"] - result["Chiv4pc"])
         return result
-
-
-_connectivity = {'Chiv0': Connectivity.calculate_chiv0,
-                 'Chiv1': Connectivity.calculate_chiv1,
-                 'Chiv2': Connectivity.calculate_chiv2,
-                 'Chiv3': Connectivity.calculate_chiv3p,
-                 'Chiv4': Connectivity.calculate_chiv4p,
-                 'Chiv5': Connectivity.calculate_chiv5p,
-                 'Chiv6': Connectivity.calculate_chiv6p,
-                 'Chiv7': Connectivity.calculate_chiv7p,
-                 'Chiv8': Connectivity.calculate_chiv8p,
-                 'Chiv9': Connectivity.calculate_chiv9p,
-                 'Chiv10': Connectivity.calculate_chiv10p,
-                 'Chi3c': Connectivity.calculate_chi3c,
-                 'Chi4c': Connectivity.calculate_chi4c,
-                 'Chi4pc': Connectivity.calculate_chi4pc,
-                 'Chi3ch': Connectivity.calculate_chi3ch,
-                 'Chi4ch': Connectivity.calculate_chi4ch,
-                 'Chi5ch': Connectivity.calculate_chi5ch,
-                 'Chi6ch': Connectivity.calculate_chi6ch,
-                 'Chi0': Connectivity.calculate_chi0,
-                 'Chi1': Connectivity.calculate_chi1,
-                 'Chi2': Connectivity.calculate_chi2,
-                 'Chi3': Connectivity.calculate_chi3p,
-                 'Chi4': Connectivity.calculate_chi4p,
-                 'Chi5': Connectivity.calculate_chi5p,
-                 'Chi6': Connectivity.calculate_chi6p,
-                 'Chi7': Connectivity.calculate_chi7p,
-                 'Chi8': Connectivity.calculate_chi8p,
-                 'Chi9': Connectivity.calculate_chi9p,
-                 'Chi10': Connectivity.calculate_chi10p,
-                 'Chiv3c': Connectivity.calculate_chiv3c,
-                 'Chiv4c': Connectivity.calculate_chiv4c,
-                 'Chiv4pc': Connectivity.calculate_chiv4pc,
-                 'Chiv3ch': Connectivity.calculate_chiv3ch,
-                 'Chiv4ch': Connectivity.calculate_chiv4ch,
-                 'Chiv5ch': Connectivity.calculate_chiv5ch,
-                 'Chiv6ch': Connectivity.calculate_chiv6ch,
-                 'mChi1': Connectivity.calculate_mean_randic,
-                 'knotp': Connectivity.calculate_delta_chi3c4pc,
-                 'dchi0': Connectivity.calculate_delta_chi0,
-                 'dchi1': Connectivity.calculate_delta_chi1,
-                 'dchi2': Connectivity.calculate_delta_chi2,
-                 'dchi3': Connectivity.calculate_delta_chi3,
-                 'dchi4': Connectivity.calculate_delta_chi4,
-                 'knotpv': Connectivity.calculate_delta_chiv3c4pc}
